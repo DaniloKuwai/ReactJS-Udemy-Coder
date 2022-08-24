933933aaaec0393a585f99fbb5cd775db2459e13
unknown <danilorkuwai@gmail.com>
2022-08-23 21:58:45 -0300

    aula 16  - Componente com Propriedade

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 
 ReactDOM.render(
     <div>
-       <Primeiro></Primeiro>     
+       <Primeiro></Primeiro>
+       <ComParametro titulo="Notas alunos" aluno="Matheus" nota="10"/>
+       <ComParametro titulo="Notas alunos" aluno="Laura" nota="8"/>     
     </div>,
     document.getElementById('root')
     )
