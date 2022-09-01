a1d640c2608cc09436808fb3731ae8a992972b07
unknown <danilorkuwai@gmail.com>
2022-09-01 00:44:06 -0300

    Aula 22 - Componente Card#01

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,6 +4,7 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento.jsx'
 import Aleatorio from './components/basicos/Aleatorio'
+import Card from './components/layout/Card.jsx'
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -11,10 +12,17 @@ export default () => {
     return (
         <div id="app">
             <h1>Fundamentos React (Arrow)</h1>
-            <Fragmento></Fragmento>
-            <ComParametro titulo="Notas alunos" aluno="Matheus" nota="10" />
-            <Primeiro></Primeiro>
-            <Aleatorio min={10} max={60} />
+            <Card titulo="Desafio  Aleatório">
+                <Aleatorio min={10} max={60} />
+            </Card>
+
+            <Card titulo="Fragmento"><Fragmento></Fragmento></Card>
+            
+            <Card titulo="ComParametro"><ComParametro titulo="Notas alunos" aluno="Matheus" nota="10" /></Card>
+            
+            <Card titulo="Primeiro Componente"><Primeiro></Primeiro></Card>
+            
+            
         </div>
     );
 }
