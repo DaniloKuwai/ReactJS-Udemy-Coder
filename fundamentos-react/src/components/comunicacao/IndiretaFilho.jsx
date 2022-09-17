5e9a32022c156815c9227a9e771d9bba3f133ad0
unknown <danilorkuwai@gmail.com>
2022-09-16 22:03:42 -0300

    aula 35 - Componente com Estado

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -3,12 +3,17 @@ import React from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
+    const cb = props.quandoClicar
+    const min = 50
+    const max = 70
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+    const gerarNerd = () => Math.random() > 0.5
     return (
         <div>
             <div>Filho</div>
             <button onClick={
                 function (e){
-                    props.quandoClicar('João',53,true)
+                    props.quandoClicar('João',gerarIdade(),gerarNerd())
                 }
             }>Fornecer Informações</button>
         </div>
